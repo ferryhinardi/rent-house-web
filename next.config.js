a43b5cbfc9f607efdf650578d967eaac326f950f
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2021-08-07 16:05:42 +0700

    add config trailing slash

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withTM = require('next-transpile-modules')(['react-native-vector-icons']);
 
 module.exports = withTM({
+  // Issue dynamic route when deploy static page in digital ocean
+  // https://www.digitalocean.com/community/questions/next-js-static-site-requires-html-extension-in-url-for-dynamic-routes
+  trailingSlash: true,
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
@@ -15,5 +19,4 @@ module.exports = withTM({
     ]
     return config
   },
-  reactStrictMode: true,
 });
