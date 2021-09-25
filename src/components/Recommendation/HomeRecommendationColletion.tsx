d385874f81ffd2dadd304ecbf86d937d09718799
Melisa Cecilia <melisa.cecilia@freshbooks.com>
2021-09-24 22:45:37 -0400

    WIP add house matching

--- src/components/Recommendation/HomeRecommendationColletion.tsx
+++ src/components/Recommendation/HomeRecommendationColletion.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { ResponseItem, House } from 'types';
+import { QUERY_KEYS } from 'core/constants';
 import { useRouter } from 'next/router';
 import { View, StyleSheet } from 'react-native';
-import { Token } from 'core';
+import { Token, fetcher } from 'core';
+import { routePaths } from 'routePaths';
 import HomeRecommendationCard from './HomeRecommendationCard';
+import { HomeRecommendationPlaceholder } from 'components/Placeholder';
 
 export default function HomeRecommendationColletion() {
   const router = useRouter();
 
+  const { data, isLoading } = useQuery<ResponseItem<House>>(
+    QUERY_KEYS.HOUSE_MATCH,
+    async () => {
+      const res = await fetcher<ResponseItem<House>>({
+        method: 'GET',
+        url: '/match-property/preferences/11',
+      });
+      return res;
+    }
+  );
+  const homeData = data?.data || [];
+  console.log(data);
+
   return (
     <View style={styles.container}>
-      <HomeRecommendationCard
-        // still hardcode ID until home recommendation integrated
-        onViewDetail={() => router.push('account/home-recommendation/2')}
-      />
-      <HomeRecommendationCard
-        // still hardcode ID until home recommendation integrated
-        onViewDetail={() => router.push('account/home-recommendation/1')}
-      />
+      {isLoading ? (
+        <HomeRecommendationPlaceholder />
+      ) : (
+        <View style={styles.container}>
+          {homeData.map((item, index) => (
+            <HomeRecommendationCard
+              onViewDetail={() => {
+                router.push({
+                  pathname: routePaths.homeDetail,
+                  query: { homeId: item.id },
+                });
+              }}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 }
