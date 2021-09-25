04677b5e7978f3e4a594ee4ffd38517f64e43ff4
Veneciac <veneciacalista00@gmail.com>
2021-09-25 16:33:00 +0700

    add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import config from 'config';
+
+// temp use imported img later will upload to digital ocean
+const assets = {
+  canadaFlag: require('./canada-flag.png'),
+  homeLineHorizontal: require('./line-horizontal.png'),
+};
+
+export default assets;
