7e2b5430eae3cc68f40549df53869e426f642bd9
ferryhinardi <hinardi93@gmail.com>
2021-10-30 07:49:11 +0000

    add render style in server side

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,39 @@
+import { Children } from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { AppRegistry } from 'react-native';
+import config from '../app.json';
+// Force Next-generated DOM elements to fill their parent's height
+const normalizeNextElements = `
+  #__next {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`
 
 class RynaDocument extends Document {
+  static async getInitialProps({ renderPage }: any) {
+    AppRegistry.registerComponent(config.name, () => Main)
+    // @ts-ignore
+    const { getStyleElement } = AppRegistry.getApplication(config.name)
+    const page = await renderPage()
+    const styles = [
+      <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
+      getStyleElement(),
+    ]
+    return { ...page, styles: Children.toArray(styles) }
+  }
+
   render() {
     return (
-      <Html>
+      <Html style={{ height: '100%' }}>
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&display=optional"
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body style={{ height: '100%', overflow: 'hidden' }}>
           <Main />
           <NextScript />
         </body>
