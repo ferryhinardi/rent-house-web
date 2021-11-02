c595fd99af5ebd422ba0f0aaa2a7ffae564d1db9
Aslam Hadi Harsono <aslamhadi@gmail.com>
2021-11-03 06:39:58 +0700

    misc: use current year in footer

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -42,7 +42,7 @@ const en = {
   contactUsEmail: 'hello@theryna.com',
   contactUsTelp: 'Tel: +1 647-370-1095',
   footerFollowUsTitle: 'Follow Us',
-  copyRightCaption: 'Copyright @ 2020 Ryna. All right reserved.',
+  copyRightCaption: 'Copyright @ {{year}} Ryna. All right reserved.',
   basicProfile: 'Basic Profile',
   accountSettings: 'Account Settings',
   recommendation: 'Recommendation',
