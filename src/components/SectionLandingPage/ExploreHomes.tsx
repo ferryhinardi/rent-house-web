e19c8ff0bf162c75aa42af39596c9151a3f33564
Aslam Hadi Harsono <aslamhadi@gmail.com>
2021-11-23 23:03:40 +0700

    feature: use dummy house if we don't have the city

--- src/components/SectionLandingPage/ExploreHomes.tsx
+++ src/components/SectionLandingPage/ExploreHomes.tsx
@@ -13,6 +13,8 @@ import { QUERY_KEYS } from 'core/constants';
 import { ExploreHomePlaceholder } from 'components/Placeholder';
 import assets from 'assets';
 
+const allCityList = ['toronto', 'montreal', 'ottawa', 'vancouver'];
+
 export default function ExploreHomes() {
   const { t } = useTranslation();
   const { data, isLoading } = useQuery<ResponseItem<House>>(QUERY_KEYS.HOUSE, async () => {
@@ -22,8 +24,43 @@ export default function ExploreHomes() {
     });
     return res;
   });
-
   const homeData = data?.data;
+
+  // mitigate if the data from API doesn't contain all required cities
+  const fillWithDummyHouses = () => {
+    const cityList = homeData?.map((h) => h.city.toLowerCase());
+    const cityNotExistInAPI = allCityList.filter((item) => !cityList?.includes(item));
+    
+    cityNotExistInAPI.map((city) => {
+      var randomNumber = Math.floor(Math.random() * (9999 - 1234 + 1)) + 1234;
+      const h: House = {
+        city: city,
+        name: 'Coming Soon',
+        lead_media: config.comingSoonImage,
+        id: randomNumber,
+        partner_id: 0,
+        address: '',
+        minimum_term_length: '',
+        galleries: [],
+        floor_plan_image: '',
+        location_lat: {
+          Float64: 0,
+          Valid: false,
+        },
+        location_lon: {
+          Float64: 0,
+          Valid: false,
+        },
+        tags: [],
+        embed_map: '',
+        description: '',
+        amenities: [],
+      };
+      homeData?.push(h);
+    });
+  };
+
+  fillWithDummyHouses();
   const [firstHome, secondHome, otherHomes] = [homeData?.[0], homeData?.[1], homeData?.slice(2)];
 
   return (
@@ -170,6 +207,7 @@ const styles = StyleSheet.create({
     marginBottom: Token.spacing.s,
     marginTop: Token.spacing.m,
     alignItems: 'center',
+    textTransform: 'capitalize',
   },
   cardContainer: {
     backgroundColor: 'transparent',
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,11 @@
 const config = {
   apiHost: process.env.NEXT_PUBLIC_API_HOST,
   appHost: process.env.NEXT_PUBLIC_APP_HOST,
-  googleAPIKey:
-    process.env.NEXT_PUBLIC_GOOGLE_API_KEY ||
-    'AIzaSyAzmQVVWpZ2iMdVO9_5awTVAECu8vz6pi0',
+  googleAPIKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || 'AIzaSyAzmQVVWpZ2iMdVO9_5awTVAECu8vz6pi0',
   googleAuthKey: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
   imageHost: process.env.NEXT_PUBLIC_IMAGE_HOST,
   fbAppId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID,
+  comingSoonImage: process.env.NEXT_DEFAULT_COMING_SOON_PLACEHOLDER || 'staging/media/image/coming-soon.jpg',
 };
 
 export default config;
