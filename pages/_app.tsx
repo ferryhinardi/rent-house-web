1d119f6e752dd95b14a8d230af5df83c123dd5e4
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2022-02-15 17:08:27 +0700

    add error log for client side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { Hydrate } from 'react-query/hydration';
-import { QueryClientProvider, QueryClient, QueryCache } from 'react-query';
+import { QueryClientProvider, useQueryErrorResetBoundary, QueryClient, QueryCache } from 'react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import { captureMessage, Severity } from '@sentry/browser';
 import Toast from 'react-native-toast-message';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { ErrorPage } from 'components';
 import createLocalization from 'utils/createLocalization';
 import NProgress from 'components/NProgress';
 import { Token } from 'core';
@@ -26,25 +29,38 @@ const queryClientOptions = {
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient(queryClientOptions));
+  const { reset } = useQueryErrorResetBoundary();
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <NProgress
-          color={Token.colors.gold}
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={3}
-          showOnShallow
-        />
-        <Toast
-          ref={(ref) => Toast.setRef(ref)}
-          // @ts-ignore
-          style={{ position: 'fixed', zIndex: 1 }}
-        />
-        <Component {...pageProps} />
-      </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ErrorBoundary
+      onReset={reset}
+      fallbackRender={({ resetErrorBoundary }) => <ErrorPage onRetry={resetErrorBoundary} />}
+      onError={(error, info) => {
+        captureMessage(error.message, {
+          extra: {
+            componentStack: info.componentStack,
+          },
+          level: Severity.Error,
+          contexts: {
+            react: info,
+            metadata: {
+              errorKind: 'page_error',
+            },
+          },
+        });
+      }}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <NProgress color={Token.colors.gold} startPosition={0.3} stopDelayMs={200} height={3} showOnShallow />
+          <Toast
+            ref={(ref) => Toast.setRef(ref)}
+            // @ts-ignore
+            style={{ position: 'fixed', zIndex: 1 }}
+          />
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
